888fa0573b38784e3d33dee95167acdb628bfa94
vedhsalian <vedh.salian@gmail.com>
2025-11-30 15:54:22 +0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,6 +1,33 @@
 import pgzrun
+from random import randint
 
-WIDTH=300
-HEIGHT=300
+TITLE="GOOD SHOT"
 
+WIDTH=500
+HEIGHT=500
+
+message=""
+
+alien=Actor('alien')
+
+def draw():
+    screen.clear()
+    screen.fill("black")
+    alien.draw()
+    screen.draw.text(message,center=(400,10), fontsize=30)
+
+def place_alien():
+    alien.x=randint(50,WIDTH-50)
+    alien.y=randint(50,HEIGHT-50)
+
+def on_mouse_down(pos):
+    global message
+    if alien.collidepoint(pos):
+        message="Good Shot"
+        place_alien()
+    else:
+        message="You Missed"
+
+
+place_alien()
 pgzrun.go()
