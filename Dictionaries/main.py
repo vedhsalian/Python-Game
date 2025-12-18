a42e1ae64462d6bce3c02d848ae49041ad388288
vedhsalian <vedh.salian@gmail.com>
2025-12-18 20:29:55 +0530

    Add files via upload

---
+++ Dictionaries/main.py
@@ -0,0 +1,28 @@
+capitals={"India":"Delhi", "USA":"Washington DC", "France":"Paris", "Sri Lanka":"Colombo"}
+
+#Printing the value of a key if present
+print (capitals["Sri Lanka"])
+
+#Printing the value of a key if it is present or not
+print (capitals.get("UK","Unknown"))
+
+#Adding new elements to a dictionary
+capitals["Afghanistan"]="Kabul"
+print (capitals["Afghanistan"])
+
+capitals["Andorra"]="Andorra La Vella"
+print (capitals["Andorra"])
+
+#Printing all keys in the dictionary
+print (capitals.keys())
+
+#Printing all values in the dictionary
+print (capitals.values())
+
+#Finding the length of the dictionary
+print (len(capitals))
+
+#Finding if a key is in the dictionary
+print ('USA' in capitals)
+
+print ('Canada' in capitals)
