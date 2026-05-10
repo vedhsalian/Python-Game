3feb2551daf9e3c8f63dc5ccece0ae49c3d2faa2
vedhsalian <vedh.salian@gmail.com>
2026-05-10 15:15:39 +0400

    Add files via upload

--- GUI/guess_the_number.py
+++ GUI/guess_the_number.py
@@ -1,5 +1,26 @@
 from tkinter import *
+from tkinter import messagebox
 import random
+
+correct=random.randint(1,20)
+attempts=0
+
+def get_result():
+    global correct,attempts
+    guess=int(e1.get())
+    if 0<guess<21 and guess<correct:
+        attempts+=1
+        answer="Guess higher"
+    elif 0<guess<21 and guess>correct:
+        attempts+=1
+        answer="Guess lower"
+    elif guess==correct:
+        answer="You guessed correct"
+        b1.config(state="disabled")
+        e1.config(state="disabled")
+    messagebox.showinfo("Result",answer)
+    l3.config(text="Attempts:{}".format(attempts))
+
 
 screen=Tk()
 screen.configure(bg="lightgrey")
@@ -15,7 +36,7 @@ l2.place(x=200,y=200)
 e1=Entry(width=30,fg="grey",bg="white")
 e1.place(x=400,y=205)
 
-b1=Button(text="Submit",width=40,height=2,fg="black",bg="yellow")
+b1=Button(text="Submit",width=40,height=2,fg="black",bg="yellow",command=get_result)
 b1.place(x=300,y=300)
 
 l3=Label(text="Attempts : 0",width=20,height=1,fg="black",bg="lightgrey",font=('TKDefaultFont',15))
